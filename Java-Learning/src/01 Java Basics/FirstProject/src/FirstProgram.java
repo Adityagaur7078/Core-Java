92ac30e3a089cc1c685883fa622065a4aac9ed9d
Adityagaur7078 <adityagaur7078@gmail.com>
2026-06-24 17:48:32 +0530

    Complete Java basics concepts and few Programming Challenges

---
+++ Java-Learning/src/01 Java Basics/FirstProject/src/FirstProgram.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Do");
+        System.out.println("something");
+        System.out.println("Interesting");
+    }
+}
