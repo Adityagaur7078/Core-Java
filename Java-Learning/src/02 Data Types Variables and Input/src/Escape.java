851ef28384df2f3be4a1216703aeb86fe270ea08
Adityagaur7078 <adityagaur7078@gmail.com>
2026-06-24 20:13:12 +0530

    Complete Java variables, data types, escape sequences and pattern programs challenge

---
+++ Java-Learning/src/02 Data Types Variables and Input/src/Escape.java
@@ -0,0 +1,29 @@
+public class Escape
+{
+    public static void main(String a[])
+    {
+        // \' is used to print single quotes
+
+        System.out.println("Hello 'Java' \"Developer\"...");
+
+
+        // \\ is used to print a single backslash
+
+        System.out.println("Hello 'Java' \\Developer\\...");
+
+
+        // \n moves the text to a new line
+
+        System.out.println("Hello Java \nDeveloper...");
+
+
+        // \t adds a horizontal tab space
+
+        System.out.println("Hello Java\tDeveloper...");
+
+
+        // \b removes one character before it (backspace)
+
+        System.out.println("HTML\bDeveloper...");
+    }
+}
---
+++ Java-Learning/src/02 Data Types Variables and Input/src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main() {
+        int Number;
+        Number = 34;
+        System.out.println(Number);
+        Number = 23;
+        System.out.println(Number);
+
+        boolean isBillioners = true;
+        System.out.println(isBillioners);
+
+        float myFloat = 5.0f;
+        System.out.println(myFloat);
+
+        double myDouble = 8.73767;
+        System.out.println(myDouble);
+
+        String wishes = "Happy Janmashtmi";
+        System.out.println(wishes);
+
+        char myCharacter = 'G';
+        System.out.println(myCharacter);
+    }
+}
